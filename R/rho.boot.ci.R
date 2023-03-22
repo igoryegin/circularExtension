4e52f44ad58d41098d71769aca1390604359bfc9
Igor <miellago98@gmail.com>
2023-03-23 01:02:00 +0600

    added rho.boot.ci() (WIP)

--- R/rho.boot.ci.R
+++ R/rho.boot.ci.R
@@ -1,7 +1,16 @@
-rho.boot.ci <- function(x, parametric = TRUE, nrepl = 10000) {
-  if(parametric) {
-    xunl <- unlist(x)
-    m <- length(xunl)
-    n <- sum(xunl)
+rho.boot.ci <- function(x, nrepl = 10000, ci = 0.95) {
+  xunl <- unlist(x)
+  n <- sum(xunl)
+  if(inherits(x, "3x3")) {
+    X2 <- sapply(apply(rmultinom(nrepl, n, xunl), 2, as.3x3),
+                 function(X) grouped.rayleigh.test(X, p.value = "asymptotic")$statistic)
   }
+  else {
+    X2 <- apply(rmultinom(nrepl, n, xunl), 2,
+                function(X) grouped.rayleigh.test(X, p.value = "asymptotic")$statistic)
+  }
+  X2 <- sqrt(X2) / (2 * n)
+  list(x = X2,
+       interval = quantile(X2, c((1 - ci) / 2, 1 - (1 - ci) / 2)),
+       conf.int = ci)
 }
