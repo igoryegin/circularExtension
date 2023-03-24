e7b828e7760cb01c87e56835a86c281bd3415182
Igor <miellago98@gmail.com>
2023-03-25 01:56:09 +0600

    bug fix in rho.grouped.boot.ci()

--- R/rho.grouped.boot.ci.R
+++ R/rho.grouped.boot.ci.R
@@ -9,8 +9,10 @@ rho.grouped.boot.ci <- function(x, nrepl = 10000, ci = 0.95) {
     X2 <- apply(rmultinom(nrepl, n, xunl), 2,
                 function(X) grouped.rayleigh.test(X, p.value = "asymptotic")$statistic)
   }
-  X2 <- sqrt(X2) / (2 * n)
+  X2 <- sqrt(X2 / (2 * n))
+  INTERVAL <- quantile(X2, c((1 - ci) / 2, 1 - (1 - ci) / 2))
   list(x = X2,
        interval = quantile(X2, c((1 - ci) / 2, 1 - (1 - ci) / 2)),
        conf.int = ci)
+  print(round(INTERVAL, 5))
 }
