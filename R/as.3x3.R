64545e18c00924f3c19b06fed438ddf08d4b36f3
Igor <miellago98@gmail.com>
2023-03-27 01:05:02 +0600

    added a warning for decimal numbers

--- R/as.3x3.R
+++ R/as.3x3.R
@@ -1,3 +1,9 @@
 as.3x3 <- function(x) {
-  structure(list(outer = x[1:8], zero = x[9]), class = "3x3")
+  if(is.numeric(x))
+    structure(list(outer = x[1:8], zero = x[9]), class = "3x3")
+  else if(any(x - trunc(x) > 0)) {
+    warning("Decimal numbers are provided. Only the integer parts of these numbers will be considered")
+    x <- trunc(x)
+    structure(list(outer = x[1:8], zero = x[9]), class = "3x3")
+  }
 }
