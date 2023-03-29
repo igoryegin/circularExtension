fbe657bad8c8b8e0e04000aa52ec3a521d3c6083
Igor <miellago98@gmail.com>
2023-03-29 21:23:18 +0600

    fixed p.value calculation, added boot.CI

--- R/grouped.rayleigh.test.R
+++ R/grouped.rayleigh.test.R
@@ -10,7 +10,8 @@
 ###########################################################
 
 
-grouped.rayleigh.test <- function(x, sym.axes = 1, p.value = c("auto", "asymptotic", "simulated")) {
+grouped.rayleigh.test <- function(x, sym.axes = 1, p.value = c("auto", "asymptotic", "simulated"),
+                                  boot.CI = NULL) {
   xunl <- unlist(x)
   if(!is.numeric(x) & !inherits(x, "3x3"))
     stop("Please provide a vector of whole numbers or a 3x3 object")
@@ -18,49 +19,45 @@ grouped.rayleigh.test <- function(x, sym.axes = 1, p.value = c("auto", "asymptot
     stop(sprintf("The number of symmetry axes should be between 1 and %d", floor(length(xunl) / 2)))
   sym.axes <- trunc(sym.axes)
   p.value <- match.arg(p.value)
-  if(!inherits(x, "3x3")) {
+  if(inherits(x, "3x3")) {
+    x.r <- x$outer
+    x.b <- x$zero
+    m <- length(x.r)
+    n <- sum(x.r)
+    cd <- outer(1:length(x.r), 1:length(x.r), `-`)
+    coefmat <- cos(2 * sym.axes * pi * cd / length(x.r))
+  }
+  else {
     if(any(x - trunc(x) > 0))
       warning("Decimal numbers are provided. Only the integer parts of these numbers will be considered")
     x <- trunc(x)
     if(sym.axes - trunc(sym.axes) > 0)
       warning(sprintf("The number of symmetry axis is not an integer. Only the integer part (%d) will be considered", trunc(sym.axes)))
-    m <- length(x)
-    n <- sum(x)
-    cd <- outer(1:length(x), 1:length(x), `-`)
-    coefmat <- cos(2 * sym.axes * pi * cd / length(x))
-  }
-  else if(inherits(x, "3x3")) {
-    m <- length(xunl)
-    n <- sum(xunl)
-    w <- rep(c(1, 0), c(length(x$outer), length(x$zero)))
-    cc <- c(cos(2 * sym.axes * pi * seq_len(length(x$outer)) / length(x$outer)),
-              cos(2 * sym.axes * pi * seq_len(length(x$zero)) / length(x$zero)))
-    ss <- c(sin(2 * sym.axes * pi * seq_len(length(x$outer)) / length(x$outer)),
-              sin(2 * sym.axes * pi * seq_len(length(x$zero)) / length(x$zero)))
-    coefmat <- w * cc %*% t(w * cc) + w * ss %*% t(w * ss)
+    x.r <- x
+    m <- length(x.r)
+    n <- sum(x.r)
+    cd <- outer(1:length(x.r), 1:length(x), `-`)
+    coefmat <- cos(2 * sym.axes * pi * cd / length(x.r))
   }
   statistic <- function(x) {
-    if(inherits(x, "3x3"))
-      as.numeric(
-        round(2/length(x$outer) * t((xunl - n/m) / sqrt(n/m)) %*% coefmat %*% (xunl - n/m) / sqrt(n/m), 5)
-      )
-    else
-      as.numeric(
+    as.numeric(
         round(2/length(x) * t((x - n/m) / sqrt(n/m)) %*% coefmat %*% (x - n/m) / sqrt(n/m), 5)
       )
   }
-  STATISTIC <- statistic(x = x)
+  STATISTIC.R <- statistic(x = x.r)
   method.asymp <- function() {
-    assign("PARAMETER", 2, envir = parent.frame())
-    assign("PVAL", 1 - pchisq(STATISTIC, 2), envir = parent.frame())
-    assign("METHOD", "Rayleigh test of circular uniformity for grouped observations", envir = parent.frame())
+    assign("PARAMETER.R", 2, envir = parent.frame())
+    assign("PVAL.R", 1 - pchisq(STATISTIC.R, 2), envir = parent.frame())
+    assign("METHOD.R", "Rayleigh test of circular uniformity for grouped observations", envir = parent.frame())
   }
   method.sim <- function() {
-    assign("PARAMETER", NA, envir = parent.frame())
+    assign("PARAMETER.R", NA, envir = parent.frame())
     mc.vectors <- rmultinom(9999, n, rep(1/m, m))
     mc.statistics <- apply(mc.vectors, 2, statistic)
-    assign("PVAL", 1 / (length(mc.statistics) + 1) * (length(which(mc.statistics >= STATISTIC)) + 1), envir = parent.frame())
-    assign("METHOD", "Rayleigh test of circular uniformity for grouped observations (simulated p-values)", envir = parent.frame())
+    assign("PVAL.R", 1 / (length(mc.statistics) + 1) * (length(which(mc.statistics >= STATISTIC.R)) + 1),
+           envir = parent.frame())
+    assign("METHOD.R", "Rayleigh test of circular uniformity for grouped observations (simulated p-values)",
+           envir = parent.frame())
   }
   if(p.value == "asymptotic") {
     method.asymp()
@@ -76,10 +73,25 @@ grouped.rayleigh.test <- function(x, sym.axes = 1, p.value = c("auto", "asymptot
       method.asymp()
     }
   }
-  INPUT <- deparse(substitute(x))
-  names(STATISTIC) <- "X2"
-  names(PARAMETER) <- "df"
-  structure(list(method = ifelse(inherits(x, "3x3"), paste("3x3", METHOD), METHOD),
-                 data.name = INPUT, statistic = STATISTIC,
-                 parameter = PARAMETER, p.value = PVAL, test.data = x), class = "htest")
+  if(!is.null(boot.CI)) {
+    CINT.R <- apply(rmultinom(9999, n, rep(1/m, m)),
+                  2,
+                  function(X) sqrt(statistic(X) / (2 * n)))
+    CINT.R <- quantile(CINT.R, c((1 - boot.CI) / 2, 1 - (1 - boot.CI) / 2))
+  }
+  else {
+    CINT.R <- NULL
+  }
+  INPUT.R <- deparse(substitute(x.r))
+  names(STATISTIC.R) <- "X2"
+  names(PARAMETER.R) <- "df"
+  attr(CINT.R, "conf.level") <- boot.CI
+  rayleigh <- structure(list(method = ifelse(inherits(x, "3x3"), paste("3x3", METHOD.R), METHOD.R),
+                             data.name = INPUT.R, statistic = STATISTIC.R, conf.int = CINT.R,
+                             parameter = PARAMETER.R, p.value = PVAL.R, test.data = x), class = "htest")
+  if(inherits(x, "3x3")) {
+    binom <- binom.test(x.b, sum(c(x.r, x.b)), p = 0.5, alternative = "less")
+    binom$data.name <- deparse(substitute(x))
+  }
+  return(list(rayleigh = rayleigh, binom = binom))
 }
