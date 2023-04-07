3a7608f8ce754901eb0271c27c70690cf2d9fd40
Igor <miellago98@gmail.com>
2023-04-07 23:51:51 +0600

    rewrote a function

--- R/datan2bunif.R
+++ R/datan2bunif.R
@@ -1,8 +1,10 @@
-datan2bunif <- function(x) {
-  ifelse((x >= -pi & x < -3 / 4 * pi) | (x >= -pi / 4 & x <= pi / 4) | (x >= 3 / 4 * pi & x <= pi),
-         1 / (8 * cos(x) ^ 2),
-         ifelse((x >= -3 / 4 * pi & x <= -pi / 4) | (x >= pi / 4 & x <= 3 / 4 * pi),
-                1 / (8 * sin(x) ^ 2),
-                0)
-  )
-}
+datan2bunif <- Vectorize(
+  function(x) {
+    if((x >= -pi & x < -3 / 4 * pi) | (x >= -pi / 4 & x <= pi / 4) | (x >= 3 / 4 * pi & x <= pi))
+      1 / (8 * cos(x) ^ 2)
+    else if((x >= -3 / 4 * pi & x <= -pi / 4) | (x >= pi / 4 & x <= 3 / 4 * pi))
+      1 / (8 * sin(x) ^ 2)
+    else
+      0
+  }
+)
