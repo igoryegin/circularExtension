52d1d8b0ce250fddf975faeff133fe156ad0f161
Igor <miellago98@gmail.com>
2023-04-12 00:13:41 +0600

    added boot CI

--- R/moore.vector.test.R
+++ R/moore.vector.test.R
@@ -5,11 +5,11 @@
 # FOR VECTOR DATA OR WEIGHTED CIRCULAR OBSERVATIONS       #
 # Author: Igor Yegin                                      #
 #                                                         #
-# Last update: 08/04/2023                                 #
+# Last update: 11/04/2023                                 #
 #                                                         #
 ###########################################################
 
-moore.vector.test <- function(x, w = NULL, p.value = c("asymptotic", "simulated"), rho.test = NULL) {
+moore.vector.test <- function(x, w = NULL, p.value = c("asymptotic", "simulated"), rho.CI = NULL, rho.binom.test = NULL) {
   require(circular)
   if(inherits(x, "3x3"))
     stop("This function does not support 3x3 objects")
@@ -18,22 +18,27 @@ moore.vector.test <- function(x, w = NULL, p.value = c("asymptotic", "simulated"
   INPUT <- deparse(substitute(x))
   p.value <- match.arg(p.value)
   if(is.list(x)) {
-    x.t <- x$theta
+    X <- x$theta
     n <- length(x$theta)
     w <- x$rho
-    w.r <- rank(x$rho)
+    W <- rank(x$rho)
   }
   else {
-    x.t <- x
+    X <- x
     n <- length(x)
-    w.r <- rank(w)
+    W <- rank(w)
   }
   statistic <- function(x, w) {
     ss <- sum(w * sin(x))
     cc <- sum(w * cos(x))
     (cc^2 + ss^2) / (n * (n + 1) * (2 * n + 1) / 12)
   }
-  STATISTIC <- statistic(x = x.t, w = w.r)
+  boot.fun <- function(data, ind) {
+    Rbar <- sqrt(weighted.mean(cos(as.numeric(data$X[ind])), as.numeric(data$W[ind])) ^ 2 +
+                   weighted.mean(sin(as.numeric(data$X[ind])), as.numeric(data$W[ind])) ^ 2)
+    Rbar
+  }
+  STATISTIC <- statistic(x = X, w = W)
   method.asymp <- function() {
     assign("METHOD", "Moore's test of circular uniformity for vector data", envir = parent.frame())
     assign("PVAL", 1 - pchisq(STATISTIC, 2), envir = parent.frame())
@@ -53,14 +58,22 @@ moore.vector.test <- function(x, w = NULL, p.value = c("asymptotic", "simulated"
   else {
     method.sim()
   }
+  if(!is.null(rho.CI)) {
+    CINT <- boot(data.frame(X = X, W = W), boot.fun, R = 10000)
+    CINT <- boot:::norm.ci(CINT, conf = rho.CI)[2:3]
+    attr(CINT, "conf.level") <- rho.CI
+    }
+  else {
+    CINT <- NULL
+  }
   names(STATISTIC) <- "X2"
   names(PARAMETER) <- "df"
   rayleigh <- structure(list(method = METHOD, data.name = INPUT,
                              statistic = STATISTIC, parameter = PARAMETER,
-                             p.value = PVAL), class = "htest")
-  if(!is.null(rho.test)) {
-    rho.success <- which(w > rho.test)
-    binom <- binom.test(length(rho.success), length(w), p = 0.5, alternative = "greater")
+                             p.value = PVAL, conf.int = CINT), class = "htest")
+  if(!is.null(rho.binom.test)) {
+    success <- which(w > rho.binom.test)
+    binom <- binom.test(length(success), length(w), p = 0.5, alternative = "greater")
     binom$data.name <- deparse(substitute(x))
     return(list(rayleigh = rayleigh, binom = binom))
   }
