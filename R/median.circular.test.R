f16bc8b23fe5319fd151375a67be3ac90c7edf05
Igor <miellago98@gmail.com>
2023-05-07 11:15:02 +0600

    fixed the simulated p-values

--- R/median.circular.test.R
+++ R/median.circular.test.R
@@ -6,19 +6,19 @@
 #                                                         #
 # Author: Igor Yegin                                      #
 #                                                         #
-# Last update: 18/04/2023                                 #
+# Last update: 07/05/2023                                 #
 #                                                         #
 ###########################################################
 
 median.circular.test <- function(x, p.value = c("asymptotic", "simulated")) {
   p.value <- match.arg(p.value)
   x.unl <- unlist(x)
+  nk <- lengths(x)
   N <- length(x.unl)
   theta.tilde <- circular:::MedianCircularRad(x.unl)
   statistic <- function(x) {
     mk <- sapply(x, function(X) length(which(X - theta.tilde < 0)))
     M <- sum(mk)
-    nk <- lengths(x)
     N ^ 2 / (M * (N - M)) * sum(mk ^ 2 / nk) - (N * M) / (N - M)
   }
   STATISTIC <- statistic(x = x)
@@ -30,9 +30,10 @@ median.circular.test <- function(x, p.value = c("asymptotic", "simulated")) {
   method.sim <- function() {
     vectrunif <- Vectorize(
       function(n)
-        runif(n, -pi, pi)
+        runif(n, -pi, pi),
+      SIMPLIFY = FALSE
     )
-    sim.vectors <- replicate(9999, vectrunif(lengths(x)), simplify = FALSE)
+    sim.vectors <- replicate(9999, vectrunif(nk), simplify = FALSE)
     sim.statistics <- sapply(sim.vectors, statistic)
     assign("PVAL", 1 / (length(sim.statistics) + 1) * (length(which(sim.statistics >= STATISTIC)) + 1), envir = parent.frame())
     assign("METHOD", "Fisher's test for common circular median (simulated p-values)", envir = parent.frame())
@@ -45,6 +46,7 @@ median.circular.test <- function(x, p.value = c("asymptotic", "simulated")) {
     method.sim()
   }
   names(STATISTIC) <- "X2"
+  names(PARAMETER) <- "df"
   INPUT <- deparse(substitute(x))
   structure(list(data.name = INPUT, method = METHOD, statistic = STATISTIC,
                  parameter = PARAMETER, p.value = PVAL), class = "htest")
