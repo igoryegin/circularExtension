814248bd177371eacd6a1c37dd84488be33da206
Igor <miellago98@gmail.com>
2023-06-19 00:22:38 +0600

    added asymptotic rho.CI

--- R/moore.vector.test.R
+++ R/moore.vector.test.R
@@ -5,12 +5,13 @@
 # FOR VECTOR DATA OR WEIGHTED CIRCULAR OBSERVATIONS       #
 # Author: Igor Yegin                                      #
 #                                                         #
-# Last update: 28/04/2023                                 #
+# Last update: 18/06/2023                                 #
 #                                                         #
 ###########################################################
 
 moore.vector.test <- function(x, w = NULL, p.value = c("asymptotic", "simulated"), 
-                              rho.CI = NULL, rho.minlength = NULL, rho.minlength.conf = 0.95) {
+                              rho.CI = c("none", "asymptotic", "bootstrap"), rho.CI.conf = 0.95, 
+                              rho.minlength = NULL, rho.minlength.conf = 0.95) {
   require(circular)
   require(boot)
   if(inherits(x, "3x3"))
@@ -19,6 +20,7 @@ moore.vector.test <- function(x, w = NULL, p.value = c("asymptotic", "simulated"
     stop("Vector of angles (x) and vector of weights/lengths (w) must have equal number of elements")
   INPUT <- deparse(substitute(x))
   p.value <- match.arg(p.value)
+  rho.CI <- match.arg(rho.CI)
   if(is.list(x)) {
     X <- x$theta
     n <- length(x$theta)
@@ -60,11 +62,27 @@ moore.vector.test <- function(x, w = NULL, p.value = c("asymptotic", "simulated"
   else {
     method.sim()
   }
-  if(!is.null(rho.CI)) {
+  if(rho.CI == "asymptotic") {
+    n <- length(X)
+    rhobar <- circular:::TrigonometricMomentRad(X, p = 1, center = FALSE)[2]
+    a <- circular:::TrigonometricMomentRad(X, p = 1, center = FALSE)[3]
+    a2 <- circular:::TrigonometricMomentRad(X, p = 2, center = FALSE)[3]
+    b <- circular:::TrigonometricMomentRad(X, p = 1, center = FALSE)[4]
+    b2 <- circular:::TrigonometricMomentRad(X, p = 2, center = FALSE)[4]
+    rhocorr <- rhobar - (2 * n + 1) / (3 * n * (n + 1)) * (1 / rhobar ^ 3) * 
+      (b^2 / 2 * (1 + a2 - 2 * a^2) - a * b * (b2 - 2*a*b) + a^2 / 2 * (1 - a2 - 2 * b^2))
+    rhose <- qnorm(rho.CI.conf) * sqrt(
+      (2 * n + 1) / (3 * n * (n + 1)) * (1 / rhobar ^ 2) * 
+        (a^2 * (1 + a2 - 2 * a^2) + 2 * a * b * (b2 - 2 * a * b) + b^2 * (1 - a2 - 2 * b^2))
+      )
+    CINT <- c(pmax(0, rhocorr - rhose), 1)
+    attr(CINT, "conf.level") <- rho.CI.conf
+  }
+  else if(rho.CI == "bootstrap") {
     CINT <- boot(data.frame(X = X, W = W), boot.fun, R = 10000)
-    CINT <- c(pmax(0, boot:::basic.ci(CINT$t0, CINT$t, conf = 2 * rho.CI - 1)[4]), 1)
-    attr(CINT, "conf.level") <- rho.CI
-    }
+    CINT <- c(pmax(0, boot:::basic.ci(CINT$t0, CINT$t, conf = 2 * rho.CI.conf - 1)[4]), 1)
+    attr(CINT, "conf.level") <- rho.CI.conf
+  }
   else {
     CINT <- NULL
   }
