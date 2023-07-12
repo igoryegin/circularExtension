66b23c565a7fdbab3adce18f5ed979bfcc1988e9
Igor <miellago98@gmail.com>
2023-07-12 22:56:50 +0600

    d/patan2bunif() are now hidden

--- R/sq2circ.R
+++ R/sq2circ.R
@@ -1,3 +1,15 @@
+###########################################################
+#                                                         #
+#                                                         #
+# SQUARE UNIFORM TO DISC UNIFORM DISTRIBUTION             #
+#                                                         #
+#                                                         #
+# Author: Igor Yegin                                      #
+#                                                         #
+# Last update: 10/07/2023                                 #
+#                                                         #
+###########################################################
+
 sq2circ <- function(x, y = NULL) {
   if(is.list(x) & length(x) == 2 & length(x[[1]]) == length(x[[2]])) {
     rho <- pmax(abs(x[[1]]), abs(x[[2]]))
@@ -11,3 +23,35 @@ sq2circ <- function(x, y = NULL) {
   y <- rho * sin(theta)
   return(list(x = x, y = y))
 }
+
+### Auxiliary functions
+
+datan2bunif <- Vectorize(
+  function(x) {
+    if((x >= -pi & x < -3 / 4 * pi) | (x >= -pi / 4 & x <= pi / 4) | (x >= 3 / 4 * pi & x <= pi))
+      1 / (8 * cos(x) ^ 2)
+    else if((x >= -3 / 4 * pi & x <= -pi / 4) | (x >= pi / 4 & x <= 3 / 4 * pi))
+      1 / (8 * sin(x) ^ 2)
+    else
+      0
+  }
+)
+
+patan2bunif <- Vectorize(
+  function(q) {
+    if(q <= -pi)
+      0
+    else if(q > -pi & q <= -3/4 * pi)
+      tan(q) / 8
+    else if(q > -3/4 * pi & q <= -1/4 * pi)
+      1/4 - (1 / tan(q)) / 8
+    else if(q > -1/4 * pi & q <= 1/4 * pi)
+      1/2 + tan(q) / 8
+    else if(q > 1/4 * pi & q <= 3/4 * pi)
+      3/4 - (1 / tan(q)) / 8
+    else if(q > 3/4 * pi & q <= pi)
+      1 + tan(q) / 8
+    else
+      1
+  }
+)
